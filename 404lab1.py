c7d7f4c4e7e9fe79c74413a2e4e9c6e8a6f99106
Mingwei Li <mingweiarthurli@gmail.com>
2020-01-09 00:25:52 -0700

    update 404lab1.txt and 404lab1.py

--- 404lab1.py
+++ 404lab1.py
@@ -1,9 +1,15 @@
 import requests
 
 def main():
+    # print out the version of the requests library
     print(requests.__version__)
 
+    # GET the Google homepage
     google = requests.get('https://google.com')
     print(google)
+
+    # download this file from GitHub and prints out its own source code from GitHub
+    var = requests.get('https://raw.githubusercontent.com/mingweiarthurli/CMPUT404-Lab1/master/404lab1.py')
+    print(var.content)
 
 main()
